2f8d388ba028028648c243d4ea119175a8e9d065
Alex Córdova <alexcordova111@gmail.com>
2022-09-18 12:02:04 -0500

    update time zone

--- settings.py
+++ settings.py
@@ -35,7 +35,9 @@ SESSION_CONFIGS = [
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en'
+
+TIME_ZONE = 'UTC'
 
 # e.g. EUR, GBP, CNY, JPY
 REAL_WORLD_CURRENCY_CODE = 'USD'
