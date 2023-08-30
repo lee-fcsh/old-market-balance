748783b8e36a5474d55db593d67baad15ae7d09e
John <aimajoke16@gmail.com>
2023-08-30 18:22:03 -0500

    solucionado espera en solitario

---
+++ tratamientos_mercados/static/tratamientos_mercados/antifail.js
@@ -0,0 +1,35 @@
+const next_Button = document.querySelector('.otree-btn-next')
+function highlighterAction() {
+        const completados = document.querySelectorAll('.table-success')
+        const alone = completados.length >= 6
+        const checked = !!localStorage.getItem('gameOver')
+        debugger
+        if(!checked && alone){
+            localStorage.setItem('gameOver', 'true')
+            location.reload()
+            return;
+        }
+        if(alone && checked){
+            localStorage.clear()
+            next_Button.click()
+            return;
+        } 
+        if(checked) localStorage.clear()
+}
+
+function highlighter() {
+    setTimeout(function() {
+        highlighterAction();
+    }, 200);
+}
+
+
+if (document.readyState == 'complete') {
+    highlighter();
+} else {
+    document.onreadystatechange = function () {
+        if (document.readyState === "complete") {
+            highlighter();
+        }
+    }
+}
